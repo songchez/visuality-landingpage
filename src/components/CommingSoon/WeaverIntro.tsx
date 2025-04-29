66bf06e7c23d777949f8ef0f6c47fc744b3117e0
songchez <tama4840@gmail.com>
2025-04-30 00:10:51 +0900

    videos

--- src/components/CommingSoon/WeaverIntro.tsx
+++ src/components/CommingSoon/WeaverIntro.tsx
@@ -51,7 +51,7 @@ export default function WeaverIntro() {
         </div>
         <div className="shadow-xl shadow-violet-900 m-5 md:m-0 overflow-hidden rounded-lg border-1 border-violet-400">
           <video controls autoPlay muted loop width="512" preload="auto">
-            <source src="/videos/langding-main-video.mp4" type="video/mp4" />
+            <source src="/videos/landing-main-video.mov" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
@@ -66,15 +66,15 @@ export default function WeaverIntro() {
           Your browser does not support the video tag.
         </video>
         <video controls autoPlay muted loop width="400">
-          <source src="/videos/landing_video_2.mp4" type="video/mp4" />
+          <source src="/videos/landing-video-3.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <video controls autoPlay muted loop width="400">
-          <source src="/videos/landing_video_1.mp4" type="video/mp4" />
+          <source src="/videos/landing-video-4.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <video controls autoPlay muted loop width="400">
-          <source src="/videos/landing_video_2.mp4" type="video/mp4" />
+          <source src="/videos/landing-video-5.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <video controls autoPlay muted loop width="400">
